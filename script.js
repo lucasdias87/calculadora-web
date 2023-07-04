1d7d5119c085852918aaf63effecf6fb55c3f253
Lucas Dias <lucas.dias1987lu@gmail.com>
2023-07-04 20:57:09 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 function displaynum(n1) {
     Calculator.text1.value = Calculator.text1.value + n1; 
   }
+
+
+  function temaClaro() {
+    document.body.style.backgroundImage = "url('img/br.avif')";
+    document.body.style.backgroundRepeat = 'no-repeat';
+    document.body.style.backgroundSize = '100vw 100vh';
+  }
+  
+  function temaEscuro() {
+    document.body.style.backgroundImage = "url('img/dark.avif')";
+    document.body.style.backgroundRepeat = 'no-repeat';
+    document.body.style.backgroundSize = '100vw 100vh';
+  }
